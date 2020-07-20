e93620ef4187b61907cb9458bbd677187b956070
Yevhenii Trochun <zheniatrochun@gmail.com>
2020-07-20 22:52:41 +0300

    added Lens tests

--- src/test/scala/com/yevhenii/playground/optics/homegrown/LensSuite.scala
+++ src/test/scala/com/yevhenii/playground/optics/homegrown/LensSuite.scala
@@ -1,5 +1,9 @@
 package com.yevhenii.playground.optics.homegrown
 
+import cats.derived.auto.eq._
+import cats.implicits._
+import org.scalatestplus.scalacheck.Checkers
+import org.scalacheck.ScalacheckShapeless._
 import com.yevhenii.playground.Structure.Person.Raw.Nested
 import com.yevhenii.playground.TestSuite
 
@@ -18,7 +22,11 @@ class LensSuite extends TestSuite {
         )
   )
 
-  test("TODO") {
-    // todo
+  test("setGet: get the same value that was set") {
+    forAll( (a: String, nested: Nested) => lens.get(lens.set(a)(nested)) shouldBe a )
+  }
+
+  test("getSet: set the same value that was gat should change nothing") {
+    forAll( (nested: Nested) => lens.set(lens.get(nested))(nested) shouldBe nested )
   }
 }
